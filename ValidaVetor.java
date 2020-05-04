1d09459ec63f62c9f3994966284c921dd711ab2a
Jonas-Windows <jonasdhein@gmail.com>
2020-05-04 20:07:35 -0300

    Vetores

---
+++ ValidaVetor.java
@@ -0,0 +1,20 @@
+/*
+ * FUP que solicite 3 números maiores que zero distintos para o usuário. 
+ * Sempre informe o usuário quando ele fornecer um número já existente no vetor.
+*/
+public class ValidaVetor{
+ 
+  public static void main(String[] args){
+   
+    int[] vetor = new int[3];
+    
+    int count = 0;
+    while(count < 3)
+      //antes = veriricar se o número já não existe no vetor
+      
+      vetor[i] = numero informado
+    }
+    
+  }
+  
+}
