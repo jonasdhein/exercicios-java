b26fb66b51f6a22f8d6a6fbb642451864f5a2a3b
Jonas-Windows <jonasdhein@gmail.com>
2020-05-04 21:47:13 -0300

    Exercícios de vetores

--- TabuadaVetor.java
+++ TabuadaVetor.java
@@ -7,17 +7,23 @@ public class TabuadaVetor{
  
   public static void main(String[] args){
     
-    int[] tabuada = new int[4];
+    String texto = "";
+    int[] tabuada = new int[2];
     for(int i = 0; i < tabuada.length; i++){
       tabuada[i] = Entrada.leiaInt("Informe um número qualquer");
     }//laço para armazenar os números das tabuadas
     
     for(int i = 0; i < tabuada.length; i++){
-      System.out.println("TABUADA DO " + tabuada[i] + ":");
+      //System.out.println("TABUADA DO " + tabuada[i] + ":");
+      texto += "\nTABUADA DO " + tabuada[i] + ":\n";
       for(int x = 1; x <= 10; x++){
-         System.out.println(x + " x " + tabuada[i] + " = " + (x  * tabuada[i]));
+        texto += x + " x " + tabuada[i] + " = " + (x  * tabuada[i] + "\n");
+        //System.out.println(x + " x " + tabuada[i] + " = " + (x  * tabuada[i]));
       }//passo para cada valor da tabuada
       
     }//laço para percorrer os números informados pelo usuário
+    
+    Entrada.mostraTexto("Resultado", texto);
+    System.out.println("Fim do programa");
   }
 }
--- ValidaVetor.java
+++ ValidaVetor.java
@@ -6,14 +6,43 @@ public class ValidaVetor{
  
   public static void main(String[] args){
    
-    int[] vetor = new int[3];
+    int[] vetor = new int[3];//tamanho 3 = indices 0, 1 e 2
     
     int count = 0;
-    while(count < 3)
-      //antes = veriricar se o número já não existe no vetor
+    int numero = 0;
+    boolean rep = false;
+    while(count < 3){
+      //antes = veriricar se o número já não existe no vetor (EXIGE UM NOVO LAÇO)
       
-      vetor[i] = numero informado
+      numero = Entrada.leiaInt("Informe um número maior que zero");
+      if(numero > 0){
+        //percorrer o vetor para ver se o número já está presente nele
+        for(int i = 0; i < vetor.length; i++){
+          if(numero == vetor[i]){//se não existe
+            rep = true;
+            break;
+          }
+        }
+        
+        if(rep == false){
+          vetor[count] = numero;
+          count += 1;
+        }
+        rep = false;
+        
+      }else{
+        Entrada.mostraTexto("Atenção", "Você informou um número inválido!"); 
+      }
+      
     }
+    
+    String resultado = "";
+    //laço para imprimir os valores do vetor
+    for(int i = 0; i < vetor.length; i++){
+      resultado += "posição " + i + " = " + vetor[i] + "\n";
+    }
+    Entrada.mostraTexto("Resultado", resultado);
+    System.out.println("Fim do programa");
     
   }
   
