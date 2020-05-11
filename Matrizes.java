e21592f74c61be5d3e7539c76d160ad877418a59
Jonas A. Dhein <jonasdhein@gmail.com>
2020-05-11 16:43:29 -0300

    Matrizes

---
+++ Matrizes.java
@@ -0,0 +1,21 @@
+public class Matrizes{
+  
+  public static void main(String[] args){
+  
+  int[][] matriz = new int[2][5];
+
+  matriz[0][0] = 1;
+  matriz[0][1] = 2;
+  matriz[0][2] = 3;
+  matriz[0][3] = 4;
+  matriz[0][4] = 5;
+  matriz[1][0] = 6;
+  matriz[1][1] = 7;
+  matriz[1][2] = 8;
+  matriz[1][3] = 9;
+  matriz[1][4] = 10;
+
+  }
+  
+}
+
