7150e967e41b41fc7340e7da0e178abd0112fddc
Jonas <jonasdhein@gmail.com>
2020-06-01 21:11:55 -0300

    Novos exemplos Labirinto

--- ExemplosLabirinto.java
+++ ExemplosLabirinto.java
@@ -4,11 +4,19 @@ public class ExemplosLabirinto{
  
     boolean atacar;
     
-    atacar = Entrada.leiaBoolean("Irá atacar?","Sim!!","Não");
-    if(atacar == true){
-      System.out.println("Atacouuuuuuu");
+    if(1 == 2){
+      atacar = Entrada.leiaBoolean("Irá atacar?","Sim!!","Não");
+      if(atacar == true){
+        System.out.println("Atacouuuuuuu");
+      }else{
+        System.out.println("Covarde"); 
+      }
     }else{
-     System.out.println("Covarde"); 
+     
+      if(atacar == true){
+        
+      }
+      
     }
     
   }
